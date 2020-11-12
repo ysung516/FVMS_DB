cb2d5259690cf09a955085bf32992dd7eb41249c
ysung516 <ysung516@gmail.com>
2020-11-12 09:30:16 +0900

    jsp 6.1_1 version

--- src/maintest.java
+++ src/maintest.java
@@ -1,38 +1,30 @@
 import java.io.IOException;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 
 
 public class maintest {
 	
-	/*
+		//WebDriver 설정
+		private WebDriver driver;
+		private WebElement element;
+		private String url;
+	    
+		//Properties 설정
+		public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
+		public static String WEB_DRIVER_PATH = "C:/chromedriver.exe";
+		public static String TEST_URL = "url 주소를 입력해주세요";
+		
 	public static void main(String[] args) {
 		 //TODO Auto-generated method stub
 		String mainURL = "http://suresofttech.hanbiro.net/ngw/app/#/sign";
 		String connectURL = "http://suresofttech.hanbiro.net/ngw/sign/auth"; 
 		String URL = "http://suresofttech.hanbiro.net/ngw/app/#/addrbook/list/0_196/";
-
-		try {
-
-			 Document doc=Jsoup.connect(URL)
-               
-                     .timeout(3000000).get();
-			 System.out.println(3);
-			 
-			 Elements element = doc.getAllElements();
-			 System.out.println(element);
-			 System.out.println(4);
-			 
-			 
-			//doc.text();
-			//doc.html();
-			//System.out.println(doc.text());
-			//System.out.println(doc.html());
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
 		}
 	}
-	*/
-}
+	
+
 
 
