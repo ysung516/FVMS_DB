baa4bce0c3ad67ee59c0963b5a8ad5ae6042282e
ysung516 <ysung516@gmail.com>
2020-11-12 10:22:47 +0900

    asd

--- src/maintest.java
+++ src/maintest.java
@@ -2,27 +2,25 @@ import java.io.IOException;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-
-
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
 
 public class maintest {
-	
-		//WebDriver 설정
-		private WebDriver driver;
-		private WebElement element;
-		private String url;
-	    
-		//Properties 설정
-		public static String WEB_DRIVER_ID = "webdriver.chrome.driver";
-		public static String WEB_DRIVER_PATH = "C:/chromedriver.exe";
-		public static String TEST_URL = "url 주소를 입력해주세요";
+
 		
 	public static void main(String[] args) {
 		 //TODO Auto-generated method stub
-		String mainURL = "http://suresofttech.hanbiro.net/ngw/app/#/sign";
-		String connectURL = "http://suresofttech.hanbiro.net/ngw/sign/auth"; 
-		String URL = "http://suresofttech.hanbiro.net/ngw/app/#/addrbook/list/0_196/";
+//		String mainURL = "http://suresofttech.hanbiro.net/ngw/app/#/sign";
+//		String connectURL = "http://suresofttech.hanbiro.net/ngw/sign/auth"; 
+//		String URL = "http://suresofttech.hanbiro.net/ngw/app/#/addrbook/list/0_196/";
+		
+		SeleniumExample test = new SeleniumExample(); 
+		test.login();
+		
 		}
+
+    
 	}
 	
 
